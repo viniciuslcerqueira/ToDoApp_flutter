7da51413f73008b9340157833f5eb2d9028e1e5d
Vinicius Cerqueira <viniciuscerqueira@MacBook-Air-de-Vinicius.local>
2025-04-18 20:35:29 -0300

    add FloatingActionButton + First version Modal

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -16,9 +16,74 @@ class _TasksScreenState extends State<TasksScreen> {
     Task(title: "Study Math", isDone: false),
   ];
 
+  late TextEditingController taskController;
+
+  @override
+  void initState() {
+    taskController = TextEditingController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true, // Faz o modal não ocupar toda a tela
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            builder: (context) {
+              return Padding(
+                padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                  left: 16,
+                  right: 16,
+                  top: 24,
+                ),
+                child: Column(
+                  mainAxisSize:
+                      MainAxisSize.min, // A chave aqui é o mainAxisSize.min
+                  children: [
+                    Text(
+                      "Adicionar nova tarefa",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextField(
+                      controller: taskController,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                        labelText: 'Título da tarefa',
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        String taskTitle = taskController.text.trim();
+                        if (taskTitle.isNotEmpty) {
+                          setState(() {
+                            tasksList.add(
+                              Task(title: taskTitle, isDone: false),
+                            );
+                          });
+                          Navigator.pop(context); // Fecha o modal
+                        }
+                      },
+                      child: Text('Adicionar'),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.grey,
